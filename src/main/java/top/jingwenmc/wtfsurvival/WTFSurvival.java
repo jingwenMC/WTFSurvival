cf668002c07a4b7d2785300788048ed8cd74118c
jingwenMC <2313055824@qq.com>
2021-02-04 21:13:17 +0800

    Initial Commit

---
+++ src/main/java/top/jingwenmc/wtfsurvival/WTFSurvival.java
@@ -0,0 +1,17 @@
+package top.jingwenmc.wtfsurvival;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WTFSurvival extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
